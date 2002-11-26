f57b0490da1cac7f231a5320144eddcdd87f2279
aegis <aegis@fea1f14b-4e95-431d-9825-d582385f3f73>
2002-11-26 10:12:30 +0000

    yay, first TCP thingie works
    git-svn-id: file:///desktop/SourceForge%20Backup/empyrean/svnrepos/trunk@12 fea1f14b-4e95-431d-9825-d582385f3f73

---
+++ prototypes/net_pygame/client.py
@@ -0,0 +1,190 @@
+import threading
+import socket
+
+import pygame
+from pygame.locals import *
+
+
+class Vec:
+    x = 0
+    y = 0
+
+    def __init__(self, X = 0, Y = 0):
+        self.x = X
+        self.y = Y
+
+    def as_tuple(self):
+        return (self.x, self.y)
+
+    def add(self, v):
+        return Vec(self.x + v.x, self.y + v.y)
+        
+    def mul(self, v):
+        return Vec(self.x * v.x, self.y * v.y)
+
+
+class Entity:
+    id  = 0
+    pos = Vec(0, 0)
+
+    def __init__(self, id):
+        self.id = id
+    
+
+class NetworkThread(threading.Thread):
+    def __init__(self, listener):
+        self.listener = listener
+        
+        threading.Thread.__init__(self)
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect(('localhost', 8765))
+
+    def run(self):
+        print "Connected to server"
+        
+        data = ''
+        while 1:
+            read = self.socket.recv(1024)
+            if len(read) == 0:
+                break
+
+            data += read.replace('\r', '')
+
+            eol_index = data.find('\n')
+            if eol_index != -1:
+                line = data[0:eol_index]
+                self.process_packet(line)
+                data = data[eol_index+1:]
+
+        print "Disconnected from server"
+        self.socket.close()
+
+    def process_packet(self, line):
+        print "Processing packet: " + line
+        self.listener.recieve(line)
+
+    def send(self, packet):
+        print "Sending: " + repr(packet)
+        self.socket.sendall(packet + '\n')
+
+
+class ServerConnection:
+    queue = []
+    lock = threading.Lock()
+    # net_thread = NetworkThread()
+
+    def __init__(self):
+        self.net_thread = NetworkThread(self)
+        self.net_thread.start()
+        self.net_thread.send('login')
+
+    def close(self):
+        if self.net_thread:
+            self.net_thread.send('logout')
+
+    def recieve(self, packet):
+        self.lock.acquire()
+        self.queue.append(packet)
+        self.lock.release()
+
+    def send(self, packet):
+        self.net_thread.send(packet)
+
+    def get_packets(self):
+        self.lock.acquire()
+        queue = self.queue
+        self.queue = []
+        self.lock.release()
+        return queue
+
+
+class Client:
+    # player
+    # shot
+    # connection
+    
+    # id          <-- id of self
+    others = {} # <-- other entities: id -> Entity
+
+    def main(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((400, 400))
+        pygame.display.set_caption('Empyrean Net Test')
+
+        clock = pygame.time.Clock()
+
+        self.player = pygame.image.load("player.png")
+        self.shot   = pygame.image.load("shot.png")
+        self.connection = ServerConnection()
+
+        while 1:
+            dt = clock.tick(60)
+
+            if self.process_events():
+                break
+            if self.process_packets():
+                break
+
+            self.draw()
+            pygame.display.flip()
+
+        self.connection.close()
+
+    def process_events(self):
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                return 1
+            elif event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    return 1
+                elif event.key == K_UP:
+                    self.connection.send('addvel 0 -1')
+                elif event.key == K_RIGHT:
+                    self.connection.send('addvel 1 0')
+                elif event.key == K_DOWN:
+                    self.connection.send('addvel 0 1')
+                elif event.key == K_LEFT:
+                    self.connection.send('addvel -1 0')
+            elif event.type == KEYUP:
+                if event.key == K_UP:
+                    self.connection.send('addvel 0 1')
+                elif event.key == K_RIGHT:
+                    self.connection.send('addvel -1 0')
+                elif event.key == K_DOWN:
+                    self.connection.send('addvel 0 -1')
+                elif event.key == K_LEFT:
+                    self.connection.send('addvel 1 0')
+
+
+    def process_packets(self):
+        packets = self.connection.get_packets()
+        for p in packets:
+            self.process_packet(p)
+
+    def process_packet(self, packet):
+        ops = packet.split()
+        if len(ops):
+            command = ops[0]
+            args = ops[1:]
+            if command == "id":
+                self.id = int(args[0])
+            elif command == "update":
+                id = int(args[0])
+                x = float(args[1])
+                y = float(args[2])
+                self.others[id].pos = Vec(x, y)
+            elif command == "added":
+                id = int(args[0])
+                self.others[id] = Entity(id)
+            elif command == "removed":
+                id = int(args[0])
+                del self.others[id]
+
+    def draw(self):
+        self.screen.fill(0)
+        for o in self.others.values():
+            pos = o.pos.add(Vec(2, 2)).mul(Vec(100, 100)).as_tuple()
+            self.screen.blit(self.player, pos)
+
+if __name__ == '__main__':
+    Client().main()
--- prototypes/net_pygame/server.py
+++ prototypes/net_pygame/server.py
@@ -1,20 +1,71 @@
 import socket
 import threading
+import time
+
+class World:
+    lock = threading.Lock()
+    connections = []
+
+    def update(self, dt):
+        self.lock.acquire()
+        for c in self.connections:
+            c.update(dt)
+        for c in self.connections:
+            c.broadcast(self.connections)
+        self.lock.release()
+
+    def add_connection(self, conn):
+        self.lock.acquire()
+        self.connections.append(conn)
+        for c in self.connections:
+            c.send('added %d' % conn.id)
+        self.lock.release()
+
+    def remove_connection(self, conn):
+        self.lock.acquire()
+        for c in self.connections:
+            c.send('removed %d' % conn.id)
+        self.connections.remove(conn)
+        self.lock.release()
+
+world = World()
+ 
+
+__last_id = 0
+def generate_id():
+    global __last_id
+    id = __last_id
+    __last_id += 1
+    return id
+
 
 class ConnectionThread(threading.Thread):
+    # socket
+    # id
+
+    logged_in = 0
+
+    x  = 0.0
+    y  = 0.0
+    vx = 0.0
+    vy = 0.0
+
     def __init__(self, socket):
         threading.Thread.__init__(self)
         self.socket = socket
+        self.id = generate_id()
 
     def run(self):
         print 'Connection!'
         print 'Peer Name: ' + str(self.socket.getpeername())
         print 'Sock Name: ' + str(self.socket.getsockname())
 
+        world.add_connection(self)
+
         data = ''
-        while not self.logged_out:
+        while 1:
             read = self.socket.recv(1024)
-            if len(read) == 0:
+            if not len(read):
                 break
 
             data += read.replace('\r', '')
@@ -22,28 +73,42 @@ class ConnectionThread(threading.Thread):
             eol_index = data.find('\n')
             if eol_index != -1:
                 line = data[0:eol_index]
-                self.process_packet(line)
                 data = data[eol_index+1:]
-        
+                if self.process_packet(line):
+                    break
+
+        world.remove_connection(self)
+
         print 'Disconnecting...'
         self.socket.close()
 
     def process_packet(self, packet):
-        if packet == "login":
-            if self.logged_in:
-                print "Can't log in twice!"
-            else:
-                self.logged_in = 1
-        elif packet == "logout":
-            if self.logged_out:
-                print "Can't log out twice!"
-            else:
-                self.logged_out = 1
-        else:
-            print "Unknown packet: " + repr(packet)
+        print "Processing packet: %s" % repr(packet)
 
-    logged_in = 0
-    logged_out = 0
+        split = packet.split()
+        if split:
+            command = split[0]
+            args = split[1:]
+            if command == "login":
+                self.logged_in = 1
+                self.send("id %d" % self.id)
+            elif command == "logout":
+                return 1
+            elif command == "addvel":
+                self.vx += float(args[0])
+                self.vy += float(args[1])
+
+    def send(self, packet):
+        print "Sending: " + repr(packet)
+        self.socket.sendall(packet + '\n')
+
+    def update(self, dt):
+        self.x += self.vx * dt
+        self.y += self.vy * dt
+
+    def broadcast(self, connections):
+        for c in connections:
+            self.send("update %d %f %f" % (c.id, c.x, c.y))
 
 
 class ListenerThread(threading.Thread):
@@ -55,13 +120,20 @@ class ListenerThread(threading.Thread):
         while s:
             ConnectionThread(s[0]).start()
             s = listener.accept()
- 
+
 
 def main():
-    thread = ListenerThread()
-    thread.start()
-    thread.join()
-        
+    ListenerThread().start()
+
+    last_update = time.time()
+    while 1:
+        now = time.time()
+        dt = now - last_update
+
+        world.update(dt)
+        time.sleep(1 / 20.0)
+
+        last_update = now
 
 if __name__ == '__main__':
     main()
