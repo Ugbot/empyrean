df287909bd0751bf5592badb6d588291e43f9455
aegis <aegis@fea1f14b-4e95-431d-9825-d582385f3f73>
2002-12-14 04:36:03 +0000

    made Types.h use NSPR instead of SDL, hopefully breaking the server's last tie to SDL
    git-svn-id: file:///desktop/SourceForge%20Backup/empyrean/svnrepos/trunk@105 fea1f14b-4e95-431d-9825-d582385f3f73

--- empyrean/src/base/Types.h
+++ empyrean/src/base/Types.h
@@ -5,22 +5,22 @@
 #define PYR_TYPES_H
 
 
-#include <SDL.h>
+#include <prtypes.h>
 
 
 namespace pyr {
 
     // in case we decouple from SDL
 
-    typedef Uint8  u8;
-    typedef Uint16 u16;
-    typedef Uint32 u32;
-    typedef Uint64 u64;
+    typedef PRUint8  u8;
+    typedef PRUint16 u16;
+    typedef PRUint32 u32;
+    typedef PRUint64 u64;
     
-    typedef Sint8  s8;
-    typedef Sint16 s16;
-    typedef Sint32 s32;
-    typedef Sint64 s64;
+    typedef PRInt8  s8;
+    typedef PRInt16 s16;
+    typedef PRInt32 s32;
+    typedef PRInt64 s64;
 
 }
 
