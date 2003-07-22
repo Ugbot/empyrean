f87d42056d959b36e7c63564775d87c598ae82ed
aegis <aegis@fea1f14b-4e95-431d-9825-d582385f3f73>
2003-07-22 03:31:48 +0000

    adding third-party-vc7 to CVS so we don't have to download it separately
    git-svn-id: file:///desktop/SourceForge%20Backup/empyrean/svnrepos/trunk@160 fea1f14b-4e95-431d-9825-d582385f3f73

---
+++ empyrean/third-party-vc7/include/gltext.h
@@ -0,0 +1,409 @@
+/* -*- Mode: C++; tab-width: 3; indent-tabs-mode: nil c-basic-offset: 3 -*- */
+// vim:cindent:ts=3:sw=3:et:tw=80:sta:
+/*************************************************************** gltext-cpr beg
+ *
+ * GLText - OpenGL TrueType Font Renderer
+ * GLText is (C) Copyright 2002 by Ben Scott
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile: gltext.h,v $
+ * Date modified: $Date: 2003-07-22 03:31:46 $
+ * Version:       $Revision: 1.1 $
+ * -----------------------------------------------------------------
+ *
+ ************************************************************ gltext-cpr-end */
+#ifndef GLTEXT_H
+#define GLTEXT_H
+
+#ifndef __cplusplus
+#  error GLText requires C++
+#endif
+
+#include <sstream>
+
+// The calling convention for cross-DLL calls in win32
+#ifdef WIN32
+#  define GLTEXT_CALL __stdcall
+#else
+#  define GLTEXT_CALL
+#endif
+
+#define GLTEXT_FUNC(ret, decl) extern "C" ret GLTEXT_CALL decl
+
+namespace gltext
+{
+   /**
+    * Base class for classes the manage their own memory using reference
+    * counting.
+    *
+    * This class was originally written by Chad Austin for the audiere project
+    * and released under the LGPL.
+    */
+   class RefCounted
+   {
+   protected:
+      /**
+       * Protected so users of recounted classes don't use std::auto_ptr or the
+       * delete operator.
+       *
+       * Interfaces that derive from RefCounted should define an inline, empty,
+       * protected destrutor as well.
+       */
+      ~RefCounted() {}
+   public:
+      /**
+       * Add a reference to this object. This will increment the internal
+       * reference count.
+       */
+      virtual void ref() = 0;
+
+      /**
+       * Remove a reference from this object. This will decrement the internal
+       * reference count. When this count reaches 0, the object is destroyed.
+       */
+      virtual void unref() = 0;
+   };
+
+   /**
+    * This defines a smart pointer to some type T that implements the RefCounted
+    * interface. This object will do all the nasty reference counting for you.
+    *
+    * This class was originally written by Chad Austin for the audiere project
+    * and released under the LGPL.
+    */
+   template< typename T >
+   class RefPtr
+   {
+   public:
+      RefPtr(T* ptr = 0)
+      {
+         mPtr = 0;
+         *this = ptr;
+      }
+
+      RefPtr(const RefPtr<T>& ptr)
+      {
+         mPtr = 0;
+         *this = ptr;
+      }
+
+      ~RefPtr()
+      {
+         if (mPtr)
+         {
+            mPtr->unref();
+            mPtr = 0;
+         }
+      }
+
+      RefPtr<T>& operator=(T* ptr)
+      {
+         if (ptr != mPtr)
+         {
+            if (mPtr)
+            {
+               mPtr->unref();
+            }
+            mPtr = ptr;
+            if (mPtr)
+            {
+               mPtr->ref();
+            }
+         }
+         return *this;
+      }
+
+      RefPtr<T>& operator=(const RefPtr<T>& ptr)
+      {
+         *this = ptr.mPtr;
+         return *this;
+      }
+
+      T* operator->() const
+      {
+         return mPtr;
+      }
+
+      T& operator*() const
+      {
+         return *mPtr;
+      }
+
+      operator bool() const
+      {
+         return (mPtr != 0);
+      }
+
+      T* get() const
+      {
+         return mPtr;
+      }
+
+   private:
+      T* mPtr;
+   };
+
+
+   /**
+    * A basic implementation of the RefCounted interface.  Derive your
+    * implementations from RefImpl<YourInterface>.
+    *
+    * This class was originally written by Chad Austin for the audiere project
+    * and released under the LGPL.
+    */
+   template< class Interface >
+   class RefImpl : public Interface
+   {
+   protected:
+      RefImpl()
+         : mRefCount(0)
+      {}
+
+      /**
+       * So the implementation can put its destruction logic in the destructor,
+       * as natural C++ code does.
+       */
+      virtual ~RefImpl() {}
+
+   public:
+      void ref()
+      {
+         ++mRefCount;
+      }
+
+      void unref()
+      {
+         if (--mRefCount == 0)
+         {
+            delete this;
+         }
+      }
+
+   private:
+      int mRefCount;
+   };
+
+
+   /// Styles of fonts supported.
+   enum FontStyle
+   {
+      PLAIN       = 0,  ///< Plain style
+      BOLD        = 1,  ///< Bold style
+      ITALIC      = 2,  ///< Italic style
+      BOLDITALIC  = 3   ///< Bold and italic. Equivalent to BOLD | ITALIC.
+   };
+
+   /// Renderer types supported
+   enum FontRendererType
+   {
+      BITMAP,
+      PIXMAP
+   };
+
+   /**
+    * Represents a particular font face.
+    */
+   class Font : public RefCounted
+   {
+   protected:
+      ~Font() {}
+
+   public:
+      /// Gets the name of this font.
+      virtual const char* getName() const = 0;
+
+      /// Gets the style of this font.
+      virtual FontStyle getStyle() const = 0;
+
+      /// Gets the point size of this font.
+      virtual int getSize() const = 0;
+
+      /**
+       * Gets the ascent of this font. This is the distance from the baseline to
+       * the top of the tallest glyph of this font.
+       */
+      virtual int getAscent() const = 0;
+
+      /**
+       * Gets the descent of this font. This is the distance from the baseline
+       * to the bottom of the the glyph that descends the most from the
+       * baseline.
+       */
+      virtual int getDescent() const = 0;
+
+      /**
+       * Gets the distance that must be placed between two lines of text. Thus
+       * the baseline to baseline distance can be computed as
+       * ascent + descent + linegap.
+       */
+      virtual int getLineGap() const = 0;
+   };
+   typedef RefPtr<Font> FontPtr;
+
+   class FontStream;
+
+   /**
+    * Interface to an object that knows how to render a font.
+    */
+   class FontRenderer : public RefCounted
+   {
+   protected:
+      ~FontRenderer() {}
+
+   public:
+      /**
+       * Renders the text in the given string as glyphs using this font
+       * renderer's current font.
+       */
+      virtual void render(const char* text) = 0;
+
+      /**
+       * Gets a stream for this renderer. All output to the stream will be sent
+       * as text to this renderer when it has been flushed.
+       */
+      virtual FontStream& getStream() = 0;
+
+      /**
+       * Computes the width of the given text string if it were to be rendered
+       * with this renderer.
+       */
+      virtual int getWidth(const char* text) = 0;
+
+      /**
+       * Sets the font this renderer should use.
+       *
+       * @param font    the new font to use
+       */
+      virtual void setFont(Font* font) = 0;
+
+      /**
+       * Gets the font this renderer should use.
+       *
+       * @return  this renderer's font
+       */
+      virtual Font* getFont() const = 0;
+   };
+   typedef RefPtr<FontRenderer> FontRendererPtr;
+
+   /**
+    * Provides a stream with which to use to provide text to a renderer.
+    */
+   class FontStream : public RefCounted
+   {
+   protected:
+      ~FontStream() {}
+
+   public:
+      /**
+       * Flushes the output in the given font stream through the renderer.
+       */
+      virtual FontStream& flush() = 0;
+
+      ///@{
+      /**
+       * Renders the value of the given type as text.
+       */
+      virtual FontStream& operator<<(long val) = 0;
+      virtual FontStream& operator<<(unsigned long val) = 0;
+      virtual FontStream& operator<<(bool val) = 0;
+      virtual FontStream& operator<<(short val) = 0;
+      virtual FontStream& operator<<(unsigned short val) = 0;
+      virtual FontStream& operator<<(int val) = 0;
+      virtual FontStream& operator<<(unsigned int val) = 0;
+      virtual FontStream& operator<<(float val) = 0;
+      virtual FontStream& operator<<(double val) = 0;
+      virtual FontStream& operator<<(long double val) = 0;
+      virtual FontStream& operator<<(char val) = 0;
+      virtual FontStream& operator<<(unsigned char val) = 0;
+      virtual FontStream& operator<<(const char* val) = 0;
+      virtual FontStream& operator<<(const unsigned char* val) = 0;
+      virtual FontStream& operator<<(FontStream& (*func)(FontStream& stream)) = 0;
+      ///@}
+   };
+   typedef RefPtr<FontStream> FontStreamPtr;
+
+   /**
+    * PRIVATE API - for internal use only
+    * Anonymous namespace containing our exported functions. They are extern "C"
+    * so we don't mangle the names and they export nicely as shared libraries.
+    */
+   namespace
+   {
+      /// Gets version information
+      GLTEXT_FUNC(const char*, GLTextGetVersion());
+
+      /// Creates a new Font by name.
+      GLTEXT_FUNC(Font*, GLTextCreateFont(
+         const char* name,
+         FontStyle style,
+         int size));
+
+      /// Creates a new FontRenderer.
+      GLTEXT_FUNC(FontRenderer*, GLTextCreateRenderer(
+         FontRendererType type));
+
+      /// Flushes the given font stream.
+      GLTEXT_FUNC(FontStream*, GLTextFlushStream(
+         FontStream* stream));
+   }
+
+   /**
+    * Gets the version string for GLText.
+    *
+    * @return  GLText version information
+    */
+   inline const char* GetVersion()
+   {
+      return GLTextGetVersion();
+   }
+
+   /**
+    * Creates a new font from the given name, style and point size.
+    *
+    * @param name    the name of a particular font face.
+    * @param style   the style constant for the Font. This can be PLAIN, BOLD,
+    *                ITALIC or BOLD|ITALIC. Any other bitwise combination of
+    *                these values will be ignored.
+    * @param size    the point size of the font.
+    */
+   inline Font* CreateFont(const char* name, FontStyle style, int size)
+   {
+      return GLTextCreateFont(name, style, size);
+   }
+
+   /**
+    * Creates a new font renderer of the given type.
+    *
+    * @param type    the type of renderer to create.
+    */
+   inline FontRenderer* CreateRenderer(FontRendererType type)
+   {
+      return GLTextCreateRenderer(type);
+   }
+
+   /**
+    * Flushes the output in the given font stream through the renderer.
+    */
+   inline FontStream& flush(FontStream& fs)
+   {
+      return *GLTextFlushStream(&fs);
+   }
+
+}
+
+#endif
---
+++ empyrean/third-party-vc7/include/gmtl/Intersection.h
@@ -0,0 +1,170 @@
+/************************************************************** ggt-head beg
+ *
+ * GGT: Generic Graphics Toolkit
+ *
+ * Original Authors:
+ *   Allen Bierbaum
+ *
+ * -----------------------------------------------------------------
+ * File:          $RCSfile: Intersection.h,v $
+ * Date modified: $Date: 2003-07-22 03:31:48 $
+ * Version:       $Revision: 1.1 $
+ * -----------------------------------------------------------------
+ *
+ *********************************************************** ggt-head end */
+/*************************************************************** ggt-cpr beg
+*
+* GGT: The Generic Graphics Toolkit
+* Copyright (C) 2001,2002 Allen Bierbaum
+*
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+*
+* This library is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, write to the Free Software
+* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*
+ ************************************************************ ggt-cpr end */
+#ifndef _GMTL_INTERSECTION_H_
+#define _GMTL_INTERSECTION_H_
+
+#include <gmtl/AABox.h>
+#include <gmtl/Point.h>
+#include <gmtl/Vec.h>
+#include <gmtl/VecOps.h>
+#include <gmtl/Math.h>
+
+namespace gmtl
+{
+   /**
+    * Tests if the given AABoxes intersect with each other. Sharing an edge IS
+    * considered intersection by this algorithm.
+    *
+    * @param box1    the first box to test
+    * @param box2    the second box to test
+    *
+    * @return  true if the boxes intersect; false otherwise
+    */
+   template<class DATA_TYPE>
+   bool intersect(const AABox<DATA_TYPE>& box1, const AABox<DATA_TYPE>& box2)
+   {
+      // Look for a separating axis on each box for each axis
+      if (box1.getMin()[0] > box2.getMax()[0])  return false;
+      if (box1.getMin()[1] > box2.getMax()[1])  return false;
+      if (box1.getMin()[2] > box2.getMax()[2])  return false;
+
+      if (box2.getMin()[0] > box1.getMax()[0])  return false;
+      if (box2.getMin()[1] > box1.getMax()[1])  return false;
+      if (box2.getMin()[2] > box1.getMax()[2])  return false;
+
+      // No separating axis ... they must intersect
+      return true;
+   }
+   
+   /**
+    * Tests if the given AABox and point intersect with each other. On an edge IS
+    * considered intersection by this algorithm.
+    *
+    * @param box    the box to test
+    * @param point  the point to test
+    *
+    * @return  true if the items intersect; false otherwise
+    */
+   template<class DATA_TYPE>
+   bool intersect( const AABox<DATA_TYPE>& box, const Point<DATA_TYPE, 3>& point )
+   {
+      // Look for a separating axis on each box for each axis
+      if (box.getMin()[0] > point[0])  return false;
+      if (box.getMin()[1] > point[1])  return false;
+      if (box.getMin()[2] > point[2])  return false;
+
+      if (point[0] > box.getMax()[0])  return false;
+      if (point[1] > box.getMax()[1])  return false;
+      if (point[2] > box.getMax()[2])  return false;
+
+      // they must intersect
+      return true;
+   }
+
+   /**
+    * Tests if the given AABoxes intersect if moved along the given paths. Using
+    * the AABox sweep test, the normalized time of the first and last points of
+    * contact.
+    *
+    * @param box1          the first box to test
+    * @param path1         the path the first box should travel along
+    * @param box2          the second box to test
+    * @param path2         the path the second box should travel along
+    * @param firstContact  set to the normalized time of the first point of contact
+    * @param secondContact set to the normalized time of the second point of contact
+    *
+    * @return  true if the items intersect; false otherwise
+    */
+   template<class DATA_TYPE>
+   bool intersect( const AABox<DATA_TYPE>& box1, const Vec<DATA_TYPE, 3>& path1,
+                   const AABox<DATA_TYPE>& box2, const Vec<DATA_TYPE, 3>& path2,
+                   DATA_TYPE& firstContact, DATA_TYPE& secondContact )
+   {
+      // Algorithm taken from Gamasutra's article, "Simple Intersection Test for
+      // Games" - http://www.gamasutra.com/features/19991018/Gomez_3.htm
+      //
+      // This algorithm is solved from the frame of reference of box1
+
+      // Get the relative path (in normalized time)
+      Vec<DATA_TYPE, 3> path = path2 - path1;
+
+      // The first time of overlap along each axis
+      Vec<DATA_TYPE, 3> overlap1(DATA_TYPE(0), DATA_TYPE(0), DATA_TYPE(0));
+
+      // The second time of overlap along each axis
+      Vec<DATA_TYPE, 3> overlap2(DATA_TYPE(1), DATA_TYPE(1), DATA_TYPE(1));
+
+      // Check if the boxes already overlap
+      if (intersect(box1, box2))
+      {
+         firstContact = secondContact = DATA_TYPE(0);
+         return true;
+      }
+
+      // Find the possible first and last times of overlap along each axis
+      for (int i=0; i<3; ++i)
+      {
+         if ((box1.getMax()[i] < box2.getMin()[i]) && (path[i] < DATA_TYPE(0)))
+         {
+            overlap1[i] = (box1.getMax()[i] - box2.getMin()[i]) / path[i];
+         }
+         else if ((box2.getMax()[i] < box1.getMin()[i]) && (path[i] > DATA_TYPE(0)))
+         {
+            overlap1[i] = (box1.getMin()[i] - box2.getMax()[i]) / path[i];
+         }
+
+         if ((box2.getMax()[i] > box1.getMin()[i]) && (path[i] < DATA_TYPE(0)))
+         {
+            overlap2[i] = (box1.getMin()[i] - box2.getMax()[i]) / path[i];
+         }
+         else if ((box1.getMax()[i] > box2.getMin()[i]) && (path[i] > DATA_TYPE(0)))
+         {
+            overlap2[i] = (box1.getMax()[i] - box2.getMin()[i]) / path[i];
+         }
+      }
+
+      // Calculate the first time of overlap
+      firstContact = Math::Max(overlap1[0], overlap1[1], overlap1[2]);
+
+      // Calculate the second time of overlap
+      secondContact = Math::Min(overlap2[0], overlap2[1], overlap2[2]);
+
+      // There could only have been a collision if the first overlap time
+      // occurred before the second overlap time
+      return firstContact <= secondContact;
+   }
+}
+
+#endif
