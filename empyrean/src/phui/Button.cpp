58da075325bcf840bf82724bb2ff6da43e121b03
janusfury <janusfury@fea1f14b-4e95-431d-9825-d582385f3f73>
2003-08-08 04:14:54 +0000

    Fixed text vertical alignment
    git-svn-id: file:///desktop/SourceForge%20Backup/empyrean/svnrepos/trunk@178 fea1f14b-4e95-431d-9825-d582385f3f73

--- empyrean/src/phui/Button.cpp
+++ empyrean/src/phui/Button.cpp
@@ -24,8 +24,8 @@
  *
  * -----------------------------------------------------------------
  * File:          $RCSfile: Button.cpp,v $
- * Date modified: $Date: 2003-08-08 02:51:24 $
- * Version:       $Revision: 1.4 $
+ * Date modified: $Date: 2003-08-08 04:14:54 $
+ * Version:       $Revision: 1.5 $
  * -----------------------------------------------------------------
  *
  ************************************************************** phui-cpr-end */
@@ -114,7 +114,7 @@ namespace phui
       //the button and render the text.
       float xLoc = std::max((width  - labelWidth) / 2.0f, 0.0f);
       float yLoc = std::max((height - fontHeight) / 2.0f, 0.0f);
-      glTranslatef(xLoc, height - yLoc, 0.0f);
+      glTranslatef(xLoc, yLoc, 0.0f);
       
       renderer->render(mText.c_str());
 
