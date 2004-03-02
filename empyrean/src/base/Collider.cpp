c1ab26f9c0dbda20d2725dab280ad391bfc5aedd
aegis <aegis@fea1f14b-4e95-431d-9825-d582385f3f73>
2004-03-02 06:18:08 +0000

    the collision detection now only happens on a specified z plane, instead of with every segment in the map
    git-svn-id: file:///desktop/SourceForge%20Backup/empyrean/svnrepos/trunk@323 fea1f14b-4e95-431d-9825-d582385f3f73

--- empyrean/src/base/Collider.cpp
+++ empyrean/src/base/Collider.cpp
@@ -7,8 +7,9 @@ namespace pyr {
 
     class SegmentExtractor : public MapVisitor {
     public:
-        SegmentExtractor(std::vector<Segment>& segments)
+        SegmentExtractor(std::vector<Segment>& segments, float z)
             : _segments(segments)
+            , _plane(z)
         {
         }
 
@@ -27,12 +28,38 @@ namespace pyr {
             Vec2f oldPosition = _currentPosition;
             _currentPosition += e->pos;
 
+            std::vector<GeometryElement::Vertex>& v = e->vertices;
+
             for (size_t i = 0; i < e->triangles.size(); ++i) {
+                // Should only need 2, but in the case of numerical instability...
+                Vec2f intersections[3];
+                int intersectionCount = 0;
+                
                 GeometryElement::Triangle& t = e->triangles[i];
-                std::vector<GeometryElement::Vertex>& v = e->vertices;
-                add(v[t[0]], v[t[1]]);
-                add(v[t[1]], v[t[2]]);
-                add(v[t[2]], v[t[0]]);
+                // For each segment in the triangle...
+                for (int j = 0; j < 3; ++j) {
+                    Vec3f v1 = v[t[j]];
+                    Vec3f v2 = v[t[(j + 1) % 3]];
+
+                    // Intersect each segment with the plane.
+                    float dz = v2[2] - v1[2];
+                    if (dz == 0) {
+                        if (v1[2] == _plane) {
+                            add(v1, v2);
+                        }
+                    } else {
+                        float u = (_plane - v1[2]) / dz;
+                        if (u >= 0 && u <= 1) {
+                            intersections[intersectionCount++] = Vec2f(
+                                v1[0] + u * (v2[0] - v1[0]),
+                                v1[1] + u * (v2[1] - v1[1]));
+                        }
+                    }
+                }
+
+                if (intersectionCount == 2) {
+                    add(intersections[0], intersections[1]);
+                }
             }
 
             _currentPosition = oldPosition;
@@ -40,13 +67,19 @@ namespace pyr {
 
     private:
         void add(const Vec3f& v1, const Vec3f& v2) {
-            Vec2f w1 = Vec2f(v1[0], v1[1]) + _currentPosition;
-            Vec2f w2 = Vec2f(v2[0], v2[1]) + _currentPosition;
-            _segments.push_back(Segment(w1, w2));
+            add(Vec2f(v1[0], v1[1]),
+                Vec2f(v2[0], v2[1]));
+        }
+
+        void add(const Vec2f& v1, const Vec2f& v2) {
+            _segments.push_back(Segment(
+                v1 + _currentPosition,
+                v2 + _currentPosition));
         }
 
         Vec2f _currentPosition;
         std::vector<Segment>& _segments;
+        float _plane;
     };
 
     CollisionData collide(float dt, const Vec2f& origPos, Vec2f& newPos, Vec2f& vel,
@@ -56,7 +89,7 @@ namespace pyr {
                             newPos + Vec2f(width / 2.0f, height));
 
         std::vector<Segment> segs;
-        SegmentExtractor extractor(segs);
+        SegmentExtractor extractor(segs, -15); /// @todo -15 is hard coded for the map we're using...
         terrain->handleVisitor(extractor);
 
         CollisionData rv;
