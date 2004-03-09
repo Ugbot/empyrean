137c4cfce0a681e3a1a3b2e859d08234cfc2f9fe
aegis <aegis@fea1f14b-4e95-431d-9825-d582385f3f73>
2004-03-09 03:23:24 +0000

    rework the get-resources script so that it runs faster and can be called directly from the SCons build for unix
    git-svn-id: file:///desktop/SourceForge%20Backup/empyrean/svnrepos/trunk@334 fea1f14b-4e95-431d-9825-d582385f3f73

--- empyrean/get-resources.sh
+++ empyrean/get-resources.sh
@@ -8,14 +8,4 @@ die() {
 ROOT=`pwd`
 
 cd resources || die
-
-# install them into the UNIX SCons build directory
-scons TARGET=$ROOT/build $1 || die
-
-case `uname` in
-    *cygwin*|*CYGWIN*)
-        # install them into the VC7 build directories
-        scons TARGET="$ROOT/vc7/bin/Debug"   $1 || die
-        scons TARGET="$ROOT/vc7/bin/Release" $1 || die
-        ;;
-esac
+scons $1 || die
