9edce894e3e09b7fa0609af575b5eab665860a3a
aegis <aegis@fea1f14b-4e95-431d-9825-d582385f3f73>
2004-06-16 08:33:43 +0000

    Some build fixes for Linux.  Unfortunately, in SuSE 9.0, the Python bindings currently ICE the compiler.
    git-svn-id: file:///desktop/SourceForge%20Backup/empyrean/svnrepos/trunk@453 fea1f14b-4e95-431d-9825-d582385f3f73

--- empyrean/src/client/IncludePython.h
+++ empyrean/src/client/IncludePython.h
@@ -5,15 +5,30 @@
 // Both NSPR and Python #define HAVE_LONG_LONG without checking to see
 // if it's already defined.  :P
 
+// Also, we shouldn't include <Python.h> directly because it checks the
+// _DEBUG and links with the debug version of Python (which isn't included
+// in the standard distribution).  Boost.Python works around this, so we
+// include that instead.
+
 
 #ifdef HAVE_LONG_LONG
  #undef HAVE_LONG_LONG
- #include <boost/python.hpp>
- #ifndef HAVE_LONG_LONG
-  #define HAVE_LONG_LONG
- #endif
-#else
- #include <boost/python.hpp>
+ #define PYR_HAVE_LONG_LONG
+#endif
+
+#ifdef _POSIX_C_SOURCE
+ #undef _POSIX_C_SOURCE
+ #define PYR__POSIX_C_SOURCE
+#endif
+
+#include <boost/python.hpp>
+
+#if defined(PYR_HAVE_LONG_LONG) && !defined(HAVE_LONG_LONG)
+ #define HAVE_LONG_LONG
+#endif
+
+#if defined(PYR__POSIX_C_SOURCE) && !defined(_POSIX_C_SOURCE)
+ #define _POSIX_C_SOURCE
 #endif
 
 
