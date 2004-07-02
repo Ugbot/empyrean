28c112f27ad0a411e487097608d949b07573c479
aegis <aegis@fea1f14b-4e95-431d-9825-d582385f3f73>
2004-07-02 12:16:11 +0000

    usleep isn't as portable as sleep
    git-svn-id: file:///desktop/SourceForge%20Backup/empyrean/svnrepos/trunk@480 fea1f14b-4e95-431d-9825-d582385f3f73

--- empyrean/scripts/start.sh
+++ empyrean/scripts/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd `dirname "$0"`/..
 build/server &
-usleep 500000  # We want the client to appear on top.
+sleep 1  # We want the client to appear on top.
 nice -n 1 build/client &
 wait
